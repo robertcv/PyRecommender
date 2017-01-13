1f5736fc08bdded79e06a223c6b6482c6d3bfa73
robertcv <robert.cvitkovic@gmail.com>
2017-01-13 19:51:20 +0100

    add db parser

--- Recommender.py
+++ Recommender.py
@@ -5,54 +5,67 @@ class Recommender(object):
         self.predictor = predictor
 
 
-    def parse_moviebase(self, file_name):
-        data = open(file_name, 'rt', encoding='utf-8')
-        self.items = []
-        self.users = []
-        self.user_ratings = []
-        mode = 'none'
-        for line in data:
-            ln = line.strip()
-            if not ln or ln[0] == '%': continue  # empty line or comment
-            if ln == '[items]':
-                # switch to parsing item data
-                mode = 'items'
-                continue
-            if ln == '[users]':
-                # switch to parsing user/rating data
-                mode = 'users'
-                iCount = len(self.items)
-                continue
-            if mode == 'items':
-                self.items.append(ln)
-            elif mode == 'users':
-                ln = ln.split(',')
-                if len(ln) != iCount + 1:  # check DB consistency
-                    print("User %s has invalid number of ratings (%d)." % (ln[0], len(self.ratings[ln[0]])))
-                self.user_ratings.append([])
-                self.users.append(ln[0])
-                for v in ln[1:]:
-                    v = v.strip()
-                    if v == '?':
-                        self.user_ratings[-1].append(0)
-                    else:
-                        self.user_ratings[-1].append(float(v))
+    def parseMovieDB(self, movie_file, ratedmovies_file):
+        movie = open(movie_file, 'rt', encoding='utf-8')
+        self.movieTitle = {}
+        self.movieID = []
+        self.movieDict = {}
+
+        first_line = movie.readline()
+        i = 0
+        for line in movie:
+            ln = line.strip().split('\t')
+            self.movieID.append(ln[0])
+            self.movieTitle[ln[0]] = ln[1]
+            self.movieDict[ln[0]] = i
+            i += 1
+
+        self.width = len(self.movieID)
+
+        #code for finding the number of users
+        """
+        ratedmovies = open(ratedmovies_file, 'rt', encoding='utf-8')
+        userSet = set()
+        first_line = ratedmovies.readline()
+        for line in ratedmovies:
+            ln = line.strip().split('\t')
+            userSet.add(ln[0])
+        """
+        #for speed its hardcoded
+        self.height = 2113 #len(userSet)
+
+        ratedmovies = open(ratedmovies_file, 'rt', encoding='utf-8')
+        self.userID = []
+        self.userRatings = np.full((self.height, self.width), np.nan)
+
+        first_line = ratedmovies.readline()
+        ln = ratedmovies.readline().split('\t')
+
+        curentU = ln[0]
+        self.userRatings[0][self.movieDict[ln[1]]] = ln[2]
+
+        j = 0
+        for line in ratedmovies:
+            ln = line.strip().split('\t')
+            if ln[0] == curentU:
+                self.userRatings[j][self.movieDict[ln[1]]] = ln[2]
             else:
-                print('Strange line in database:')
-                print(line)
-        # convert to numpy
-        self.user_ratings = np.array(self.user_ratings, dtype=np.int8)
+                self.userID.append(curentU)
+                self.userRatings[j][self.movieDict[ln[1]]] = ln[2]
+                curentU = ln[0]
+                j += 1
+
+        self.userID.append(curentU)
 
     def learn(self):
-        self.predictor.fit(self.user_ratings)
+        self.predictor.fit(self.userRatings)
 
-    def recommend(self, user, n=10, rec_seen=True):
+    '''
+        def recommend(self, user, n=10, rec_seen=True):
         user_id = self.users.index(user)
         rec_tup = np.array(list(zip(self.items, self.predictor.predict(user_id))))
-        rec_bool = np.logical_not(np.array(self.user_ratings[user_id], dtype=bool)) | rec_seen
+        rec_bool = np.logical_not(np.array(self.userRatings[user_id], dtype=bool)) | rec_seen
         rec = rec_tup[rec_bool]
         return rec[rec[:,1].argsort()][-n:]
 
-
-    def __str__(self):
-        return str(self.items) + "\n" + str(self.users) + "\n" + str(self.user_ratings)
+    '''
--- test.py
+++ test.py
@@ -1,11 +1,7 @@
 from Recommender import Recommender
 from Predictors import RandomPredictor
-
-
 rp = RandomPredictor(1, 5)
 r = Recommender(rp)
-r.parse_moviebase("moviebase2016.txt")
-r.learn()
-print(r.recommend("sasha", n=2))
-print(r.recommend("sasha"))
-print(r.recommend("sasha", rec_seen=False))
+r.parseMovieDB("db/movies.dat", "db/user_ratedmovies-timestamps.dat")
+print(r.userID)
+print(r.userRatings)
